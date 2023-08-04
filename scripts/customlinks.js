a02409380fda0ec12c551a536612f4ee6488a723
ayushtripathy <63307858+Ayush-Tripathy@users.noreply.github.com>
2023-08-04 20:52:04 +0530

    Update customlinks

--- scripts/customlinks.js
+++ scripts/customlinks.js
@@ -2,20 +2,6 @@ const btnSendMessage = document.getElementById("btn-send-message");
 const message = document.getElementById("messageBody");
 const path = document.getElementById("custom-link-path");
 
-
-// if (btnSendMessage != null) {
-//     btnSendMessage.onclick = () => {
-
-//         const url = "http://localhost:3001/custompath/" + path.innerHTML.trim() + "?serial=" + btnSendMessage.value;
-
-//         postData(url, { directedMessage: message.value }).then((data) => {
-//             //window.location.replace("/");
-//         });
-
-//     }
-// }
-
-
 async function postData(url = "", data) {
 
     const response = await fetch(url, {
